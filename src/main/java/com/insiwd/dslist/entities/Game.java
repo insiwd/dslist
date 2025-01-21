4864a257a8890c8895b6bf597300061adf919ddb
ruan <ruan.cavalcante505@gmail.com>
2025-01-21 10:36:57 -0300

    Game seed, corrected misspell

--- src/main/java/com/insiwd/dslist/entities/Game.java
+++ src/main/java/com/insiwd/dslist/entities/Game.java
@@ -18,14 +18,21 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    
     // year é uma palavra reservada do BD
     // precisamos mudar o nome da coluna no BANCO
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    // esse campo não pode apenas ser uma String do java, precisamos de uma instrução, para que a JPA gere como TEXTO, não VARCHAR(255)
+    // anotamos os dois
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     // construtor vazio
@@ -34,12 +41,13 @@ public class Game {
     }
 
     // construtor com argumentos
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -79,12 +87,19 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platform) {
+        this.platforms = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
